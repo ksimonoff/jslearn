54cf35574899d473851f01ba4450a1091ef4cbc1
ksimonoff <simonovake@gmail.com>
2023-03-25 06:09:16 +0300

    MOD:add lesson4 files

---
+++ lesson4/lesson4iftask.js
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<script>
+"use strict";
+
+let year = prompt('Какое официальное название языка Javascript?', '');
+
+if (year == 'ECMAScript') {
+  alert( 'Верно!' );
+} else {
+  alert( 'Не знаете? ECMAScript' ); // любое значение, кроме 2015
+}
+</script>
